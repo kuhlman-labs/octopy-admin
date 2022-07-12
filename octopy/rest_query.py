3767bfc6e22db6fb9af79cdcc5308479bb052ca5
Brett Kuhlman <kuhlman-labs@github.com>
2022-07-12 10:55:18 -0400

    adding script for updating string in webhook url

--- octopy/rest_query.py
+++ octopy/rest_query.py
@@ -39,3 +39,80 @@ class RestPostProducer(RestRequest):
         payload = {"login": user.get("login"), "email": user.get("email")}
         response = self._execute(self._method, url, payload)
         return print(response.json())
+
+class RestPatchProducer(RestRequest):
+    def __init__(self):
+        super().__init__()
+        self._method = "Patch"
+
+    def update_repo_webhook_url(self, owner, repo, hook_id, new_url):
+        url = self._base_url + f"/repos/{owner}/{repo}/hooks/{hook_id}/config"
+        payload = {"url": new_url}
+        response = self._execute(self._method, url, payload)
+        return print(response.json())
+    
+    def update_org_webhook_url(self, owner, hook_id, new_url):
+        url = self._base_url + f"/orgs/{owner}/hooks/{hook_id}/config"
+        payload = {"url": new_url}
+        response = self._execute(self._method, url, payload)
+        return print(response.json())
+
+class RestGetProvider(RestRequest):
+    def __init__(self):
+        super().__init__()
+        self._method = "Get"
+
+    def get_user(self, user):
+        url = self._base_url + f"/users/{user}"
+        response = self._execute(self._method, url, None)
+        return print(response.json())
+
+    def get_org(self, org):
+        url = self._base_url + f"/orgs/{org}"
+        response = self._execute(self._method, url, None)
+        return print(response.json())
+
+    def get_repo(self, owner, repo):
+        url = self._base_url + f"/repos/{owner}/{repo}"
+        response = self._execute(self._method, url, None)
+        return print(response.json())
+
+    def get_repo_list(self, owner):
+        url = self._base_url + f"/orgs/{owner}/repos"
+        response = self._execute(self._method, url, None)
+        return print(response.json())
+
+    def get_collaborators_permission_list(self, owner, repo):
+        url = self._base_url + f"/repos/{owner}/{repo}/collaborators"
+        response = self._execute(self._method, url, None)
+        return print(response.json())
+
+    def get_collaborator_permission(self, owner, repo, user):
+        url = self._base_url + f"/repos/{owner}/{repo}/collaborators/{user}"
+        response = self._execute(self._method, url, None)
+        return print(response.json())
+
+    def get_collaborator_permission_list(self, owner, repo):
+        url = self._base_url + f"/repos/{owner}/{repo}/collaborators"
+        response = self._execute(self._method, url, None)
+        return print(response.json())
+
+    def get_repo_webhook_list(self, owner, repo):
+        url = self._base_url + f"/repos/{owner}/{repo}/hooks"
+        response = self._execute(self._method, url, None)
+        return print(response.json())
+
+    def get_org_webhook_list(self, org):
+        url = self._base_url + f"/orgs/{org}/hooks"
+        response = self._execute(self._method, url, None)
+        return response.json()
+    
+    def get_repo_webhook(self, owner, repo, hook_id):
+        url = self._base_url + f"/repos/{owner}/{repo}/hooks/{hook_id}"
+        response = self._execute(self._method, url, None)
+        return print(response.json())
+    
+    def get_org_webhook(self, org, hook_id):
+        url = self._base_url + f"/orgs/{org}/hooks/{hook_id}"
+        response = self._execute(self._method, url, None)
+        return response.json()
---
+++ scripts/update_string_in_webhook_url.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+from dotenv import load_dotenv
+
+from octopy.rest_query import RestPatchProducer, RestGetProvider, RestRequestError
+
+config = load_dotenv()
+restpatch = RestPatchProducer()
+restget = RestGetProvider()
+
+def replace_string_in_org_webhooks_url(org, string_to_replace, new_string):
+    try:
+        webook_org_list = restget.get_org_webhook_list(org)
+        for webhook in webook_org_list:
+            webhook_url =  webhook.get("config").get("url")
+            if string_to_replace in webhook_url:
+                new_webhook_url = webhook_url.replace(string_to_replace, new_string)
+                print(f"Updating webhook url: {webhook_url} -> {new_webhook_url}")
+                restpatch.update_org_webhook_url(org, webhook["id"], new_webhook_url)
+    except RestRequestError as e:
+        print(e)
+
+replace_string_in_org_webhooks_url("Engineering", "staging", "production")
